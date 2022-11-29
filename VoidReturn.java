6eb33ef098b1dfe0f5049cef5520930842a3805f
15718 <aden.bradshaw@vtsd.com>
2022-11-29 13:11:53 -0500

    q2346qt

--- VoidReturn.java
+++ VoidReturn.java
@@ -1,7 +1,22 @@
-public class VoidReturn {
+import java.util.Scanner;
 
-   public static void main(String args[]){
-       
+public class VoidReturn {
+    public static void main(String args[]){
+        int total = addNums();
+
+        System.out.println("Your total was: " + total);
+   }
+
+
+   public static int addNums(){
+       System.out.println("we are going to add two numbers");
+       System.out.println("Please enter them below");
+
+       Scanner scan = new Scanner(System.in);
+       int num1 = scan.nextInt();
+       int num2 = scan.nextInt();
+
+       return (num1 + num2);
 
    }
 
